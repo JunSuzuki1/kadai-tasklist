c3f4ffc7a3b5ef9bd705e50a25ccca6e9b92acc4
jun.suzuki <r201301461kl@gmail.com>
2020-10-19 11:53:40 +0000

    navbar2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
         @include('commons.navbar')
         
         <div class="container">
+            
+            @include('commons.error_messages')
+            
             @yield('content')
         </div>
 
